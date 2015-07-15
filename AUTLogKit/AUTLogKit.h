7409777124a590d931db058853337fdc4df178ca
Sylvain Rebaud <sylvain@automatic.com>
2015-07-14 17:01:49 -0700

    Setup complete

---
+++ AUTLogKit/AUTLogKit.h
@@ -0,0 +1,19 @@
+//
+//  AUTLogKit.h
+//  AUTLogKit
+//
+//  Created by Sylvain Rebaud on 7/14/15.
+//  Copyright (c) 2015 Automatic Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AUTLogKit.
+FOUNDATION_EXPORT double AUTLogKitVersionNumber;
+
+//! Project version string for AUTLogKit.
+FOUNDATION_EXPORT const unsigned char AUTLogKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AUTLogKit/PublicHeader.h>
+
+
